bcf300d226cc1206e981519c2d4fb746f9a34197
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-05-06 15:00:49 +0300

    initial project structure

---
+++ nesemu1.cpp
@@ -0,0 +1,13 @@
+#include "nesemu1.h"
+#include <stdint.h>
+#include <signal.h>
+#include <assert.h>
+#include <cmath>
+
+#include <SDL2/SDL.h>
+#include <vector>
+
+int main() {
+    return 0;
+}
+
---
+++ nesemu1.h
@@ -0,0 +1,8 @@
+//
+// Created by Valery Piashchynski on 2019-05-06.
+//
+
+#ifndef NES_EMULATOR_NESEMU1_H
+#define NES_EMULATOR_NESEMU1_H
+
+#endif //NES_EMULATOR_NESEMU1_H
